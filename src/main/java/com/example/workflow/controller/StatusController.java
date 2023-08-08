c2622f062484fe431385ac69202bff89dcd8a353
parameswara vangala <parameswara.vangala@gmail.com>
2023-08-09 04:08:13 +0530

    add status endpoint

---
+++ src/main/java/com/example/workflow/controller/StatusController.java
@@ -0,0 +1,14 @@
+package com.example.workflow.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+
+    @GetMapping("/status")
+    public String status(){
+        return "Its running!";
+    }
+}
